79ba6e612510cca75f880e195578dad94748e279
Vivixing <echeverri.vivian1120@gmail.com>
2025-06-20 12:59:57 -0500

    Correción header perfiltab

--- src/app/tab-perfil/tab-perfil.module.ts
+++ src/app/tab-perfil/tab-perfil.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { TabPerfilPageRoutingModule } from './tab-perfil-routing.module';
-
 import { TabPerfilPage } from './tab-perfil.page';
 
 @NgModule({
