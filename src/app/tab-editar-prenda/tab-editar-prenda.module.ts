f640aebf4feeea0f64d0e0dd32f49a85264c5987
Vivixing <echeverri.vivian1120@gmail.com>
2025-06-29 20:39:12 -0500

    Enrutado componente a tab de editar prenda

--- src/app/tab-editar-prenda/tab-editar-prenda.module.ts
+++ src/app/tab-editar-prenda/tab-editar-prenda.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { TabEditarPrendaPageRoutingModule } from './tab-editar-prenda-routing.module';
 
 import { TabEditarPrendaPage } from './tab-editar-prenda.page';
+import { EditarPrendaModule } from "../editar-prenda/editar-prenda.module";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    TabEditarPrendaPageRoutingModule
-  ],
+    TabEditarPrendaPageRoutingModule,
+    EditarPrendaModule
+],
   declarations: [TabEditarPrendaPage]
 })
 export class TabEditarPrendaPageModule {}
