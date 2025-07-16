45637efe8c165565000f6061887cb2064e2e2126
Vivixing <echeverri.vivian1120@gmail.com>
2025-07-16 15:21:04 -0500

    Corregida rutas del pages en routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,31 +4,31 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: 'tab-perfil',
-    loadChildren: () => import('./tab-perfil/tab-perfil.module').then( m => m.TabPerfilPageModule)
+    loadChildren: () => import('./pages/tab-perfil/tab-perfil.module').then( m => m.TabPerfilPageModule)
   },
   {
     path: 'tab-closet',
-    loadChildren: () => import('./tab-closet/tab-closet.module').then( m => m.TabClosetPageModule)
+    loadChildren: () => import('./pages/tab-closet/tab-closet.module').then( m => m.TabClosetPageModule)
   },
   {
     path: 'tab-recomendacion',
-    loadChildren: () => import('./tab-recomendacion/tab-recomendacion.module').then( m => m.TabRecomendacionPageModule)
+    loadChildren: () => import('./pages/tab-recomendacion/tab-recomendacion.module').then( m => m.TabRecomendacionPageModule)
   },
   {
     path: 'tab-visualizacion',
-    loadChildren: () => import('./tab-visualizacion/tab-visualizacion.module').then( m => m.TabVisualizacionPageModule)
+    loadChildren: () => import('./pages/tab-visualizacion/tab-visualizacion.module').then( m => m.TabVisualizacionPageModule)
   },
   {
     path: 'tab-agregar-prenda',
-    loadChildren: () => import('./tab-agregar-prenda/tab-agregar-prenda.module').then( m => m.TabAgregarPrendaPageModule)
+    loadChildren: () => import('./pages/tab-agregar-prenda/tab-agregar-prenda.module').then( m => m.TabAgregarPrendaPageModule)
   },
   {
     path: 'tab-editar-prenda',
-    loadChildren: () => import('./tab-editar-prenda/tab-editar-prenda.module').then( m => m.TabEditarPrendaPageModule)
+    loadChildren: () => import('./pages/tab-editar-prenda/tab-editar-prenda.module').then( m => m.TabEditarPrendaPageModule)
   }
 ];
 @NgModule({
