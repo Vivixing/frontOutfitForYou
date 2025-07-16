c47d58e030b223a0798222520e88dd03b0ec8c5d
Vivixing <echeverri.vivian1120@gmail.com>
2025-07-16 16:18:13 -0500

    Arreglado standalone de login

--- src/app/pages/tab-login/tab-login.page.ts
+++ src/app/pages/tab-login/tab-login.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-tab-login',
   templateUrl: './tab-login.page.html',
   styleUrls: ['./tab-login.page.scss'],
+  standalone: false,
 })
 export class TabLoginPage implements OnInit {
 
