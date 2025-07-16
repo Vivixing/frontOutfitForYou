210fe275b4c03c4b1f1a1586f380dbf56584d9fd
Vivixing <echeverri.vivian1120@gmail.com>
2025-07-16 18:15:25 -0500

    Creada lógica de navegación en page de recomendación

--- src/app/pages/tab-recomendacion/tab-recomendacion.page.ts
+++ src/app/pages/tab-recomendacion/tab-recomendacion.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab-recomendacion',
@@ -8,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabRecomendacionPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
   }
-
+  goBack() {
+    this.navCtrl.back();
+  }
 }
