0ed6411e18dbf66e9f511eafe04d0a3dc2fbbbc7
Vivixing <echeverri.vivian1120@gmail.com>
2025-07-16 19:22:56 -0500

    Creado auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
+import { IonicStorageModule } from '@ionic/storage-angular';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private usuarioLogueado = false;
+  private idUsuario = '';
+
+  constructor(private storage: Storage, private router: Router) { 
+    this.iniciar();
+  }
+
+  async iniciar() {
+    await this.storage.create();
+    this.idUsuario = await this.storage.get('idUsuario');
+  }
+
+  async esLogueado(): Promise<boolean> {
+    this.idUsuario = await  this.storage.get('idUsuario')
+    this.usuarioLogueado = this.idUsuario ? true : false;
+    return this.usuarioLogueado;
+  }
+
+  idUsuarioLogueado(): string {
+    return this.idUsuario;
+  }
+
+  async login(idUser:string){
+    this.usuarioLogueado = true;
+    this.idUsuario = idUser;
+    await this.storage.set('idUsuario', idUser);
+    console.log('Desde el front: Usuario logueado', this.idUsuario);
+  }
+
+  logout(): void {
+    this.usuarioLogueado = false;
+    this.idUsuario = '';
+    this.storage.remove('idUsuario');
+    this.router.navigate(['/login']);
+  }
+}
