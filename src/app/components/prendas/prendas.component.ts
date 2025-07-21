4a2320548b245b83022185ace94a1b041c28a430
Vivixing <echeverri.vivian1120@gmail.com>
2025-07-21 12:05:11 -0500

    Terminando visualización de prendas

--- src/app/components/prendas/prendas.component.ts
+++ src/app/components/prendas/prendas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input  } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,6 +8,8 @@ import { Router } from '@angular/router';
   standalone: false,
 })
 export class PrendasComponent  implements OnInit {
+
+  @Input() prendas: any[] = [];
 
   constructor(private router:Router) { }
 
--- src/app/pages/tab-closet/tab-closet.page.ts
+++ src/app/pages/tab-closet/tab-closet.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { PrendaService } from 'src/app/services/prenda.service';
 
 @Component({
   selector: 'app-tab-closet',
@@ -10,13 +11,30 @@ import { NavController } from '@ionic/angular';
 })
 export class TabClosetPage implements OnInit {
 
-  constructor(private router: Router, private navCtrl: NavController) { }
+  prendas: any[] = [];
+
+  constructor(private router: Router, private navCtrl: NavController, private prendaService: PrendaService) { }
 
   ngOnInit() {
   }
-  
+
   goBack() {
     this.navCtrl.back();
+  }
+
+  async cargarPrendas(){
+    const userId = localStorage.getItem('user_id');
+    console.log('Usuario obtenido por ID', userId);
+    if(!userId) return;
+
+    try{
+      const response = await this.prendaService.obtenerPrendasPorIdUsuario(userId);
+      this.prendas = response.data;
+      console.log("Lista de prendas por Id usuario", this.prendas);
+    }catch(error){
+      console.error('Error al cargar prendas:', error);
+    }
+
   }
 
   irAtabAgregarPrenda(){
