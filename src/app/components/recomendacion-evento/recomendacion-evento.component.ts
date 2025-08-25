d5ecbd4d158434800bc62598ba202eb078645121
Vivixing <echeverri.vivian1120@gmail.com>
2025-08-24 19:28:40 -0500

    Completado visualización del resultado de la recomendación

--- src/app/components/recomendacion-evento/recomendacion-evento.component.ts
+++ src/app/components/recomendacion-evento/recomendacion-evento.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit} from '@angular/core';
+import { Component,EventEmitter,Input, OnInit, Output} from '@angular/core';
 import { Router } from '@angular/router';
+import { every } from 'rxjs';
 
 @Component({
   selector: 'app-recomendacion-evento',
@@ -9,10 +10,26 @@ import { Router } from '@angular/router';
 })
 export class RecomendacionEventoComponent  implements OnInit {
 
+  @Input() vestuarioSugerido: any;
+  @Input() imagenUsuario: string | null = null;
+
+  @Output() capturarImagenUsuarioEvent = new EventEmitter<any>()
+  
+  prendaSuperior : any
+  prendaInferior : any
+
   constructor(private router: Router) { }
 
   ngOnInit() {
+    if(this.vestuarioSugerido?.data){
+      this.prendaSuperior = this.vestuarioSugerido.data.find((p:any) => p.tipoPrendaId?.categoria === 'Superior');
+      this.prendaInferior = this.vestuarioSugerido.data.find((p:any) => p.tipoPrendaId?.categoria === 'Inferior');
+    }
+    console.log("Vestuario recibido en el hijo:", this.vestuarioSugerido.data);
+  }
 
+  cargarImagenUsuario(event:any){
+    this.capturarImagenUsuarioEvent.emit(event);
   }
 
   irAtabVisualizacion(){
--- src/app/pages/tab-recomendacion/tab-recomendacion.page.ts
+++ src/app/pages/tab-recomendacion/tab-recomendacion.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
@@ -11,12 +12,36 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
 export class TabRecomendacionPage implements OnInit {
 
   vestuarioSugerido: any[] = [];
+  imagenUsuario: string | null = null;
 
-  constructor(private navCtrl: NavController, private localStorageService:LocalStorageService) { }
+  constructor(private navCtrl: NavController, private localStorageService:LocalStorageService, private alertController: AlertController) { }
+
+  async showAlert(error: string) {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      subHeader: error,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 
   ngOnInit() { 
     this.vestuarioSugerido = this.localStorageService.getItem('vestuarioSugerido');
     console.log('Vestuario recuperado', this.vestuarioSugerido);
+  }
+
+  seleccionarArchivo(event:any){
+    const archivo: File = event.target.files[0];
+    if(archivo){
+      const lector = new FileReader();
+      lector.onload = () => {
+        this.imagenUsuario = lector.result as string;
+        console.log('Imagen cargada:',this.imagenUsuario);
+      };
+      lector.readAsDataURL(archivo);
+    }else{
+      this.showAlert('❌ Por favor selecciona un archivo de imagen válido')
+    }
   }
 
   goBack() {
