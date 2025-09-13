7f498860a59dbbba87d737c1ccaa361ae72de450
Vivixing <echeverri.vivian1120@gmail.com>
2025-09-13 11:21:56 -0500

    Corregido conexión con service de allertcontroller

--- src/app/pages/tab-closet/tab-closet.page.ts
+++ src/app/pages/tab-closet/tab-closet.page.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
 import { PrendaService } from 'src/app/services/prenda.service';
 import { AuthService } from 'src/app/services/auth.service';
-import { AlertController } from '@ionic/angular';
+import { UiService } from 'src/app/services/ui.service';
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab-closet',
@@ -15,7 +15,13 @@ export class TabClosetPage implements OnInit {
 
   prendas: any[] = [];
 
-  constructor(private router: Router, private navCtrl: NavController, private prendaService: PrendaService, private authService:AuthService,  private alertController: AlertController) { }
+  constructor(
+    private router: Router, 
+    private navCtrl: NavController, 
+    private prendaService: PrendaService, 
+    private authService:AuthService,  
+    private uiService: UiService  
+  ) { }
 
   ngOnInit() {}
 
@@ -23,24 +29,16 @@ export class TabClosetPage implements OnInit {
     this.cargarPrendas();
   }
 
-  async showAlert(error: string) {
-    const alert = await this.alertController.create({
-      header: 'Error',
-      subHeader: error,
-      buttons: ['OK'],
-    });
-    await alert.present();
-  }
-
   goBack() {
     this.navCtrl.back();
   }
 
   async cargarPrendas(){
+    
     const usuarioId = this.authService.idUsuarioLogueado();
     console.log('Usuario obtenido por ID', usuarioId);
     if(!usuarioId) {
-      this.showAlert('No se encontró el id del usuario')
+      this.uiService.showAlert('No se encontró el id del usuario')
     }
 
     try{
