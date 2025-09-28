24b3b81c2b36b68e89edb2b04545ca10c0b32c1d
Vivixing <echeverri.vivian1120@gmail.com>
2025-09-28 17:43:19 -0500

    Conexión a proxy

--- src/app/config/ur.api.ts
+++ src/app/config/ur.api.ts
@@ -1,2 +1,3 @@
-export const URL_BASE_API_BACK = 'http://localhost:8035'
+export const URL_BASE_API_BACK = 'https://proxyfastapi.onrender.com'
+//'http://localhost:8035'
 //'http://93.127.213.95:8035'
